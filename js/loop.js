43a12a5fd265b69eadf8d1d72123a6fb1657bbb7
[Ryan Myers] <[ryanmyers1800@icloud.com]>
2021-03-22 14:42:08 -0500

    added rank to mountains

--- js/loop.js
+++ js/loop.js
@@ -3,9 +3,11 @@ let html = ' ';
 for ( let i = 0; i < mountains.length; i++ ) {
     let mount = mountains[i];
     html += `
-    <h2>${mount.name}</h2>
-    <h3>Mountain Range:${mount.Range}</h3>
-    <h3>Elevation:${mount.Elevation}</h3>
+    <div>
+    <h2>${mount.rank}. ${mount.name}</h2>
+    <h3>Mountain Range: ${mount.Range}</h3>
+    <h3>Elevation: ${mount.Elevation}</h3>
+    </div>
     `;
 }
 
--- js/mountains.js
+++ js/mountains.js
@@ -1,12 +1,12 @@
 const mountains = [
-    {name:'Denali', Range:'Alaska Range', Elevation:'20,310 ft.'},
-    {name:'Mount Saint Elias', Range:'Saint Elias Mountains', Elevation:'18,009 ft.'},
-    {name:'Mount Foraker', Range:'Alaska Range', Elevation:'17,400 ft.'},
-    {name:'Mount Bona', Range:'Saint Elias Mountains', Elevation:'16,500 ft.'},
-    {name:'Mount Blackburn', Range:'Wrangell Mountains', Elevation:'16,390 ft'},
-    {name:'Mount Sanford', Range:'Wrangell Mountains', Elevation:'16,237 ft.'},
-    {name:'Mount Fairweather', Range:'Saint Elias Mountains', Elevation:'15,325 ft.'},
-    {name:'Mount Hubbard', Range:'Saint Elias Mountains', Elevation:'14,951 ft.'},
-    {name:'Mount Bear', Range:'Saint Elias Mountains', Elevation:'14,831'},
-    {name:'Mount Hunter', Range:'Alaska Range', Elevation:'14,573 ft.'}
+    {rank:1, name:'Denali', Range:'Alaska Range', Elevation:'20,310 ft.'},
+    {rank:2, name:'Mount Saint Elias', Range:'Saint Elias Mountains', Elevation:'18,009 ft.'},
+    {rank:3, name:'Mount Foraker', Range:'Alaska Range', Elevation:'17,400 ft.'},
+    {rank:4, name:'Mount Bona', Range:'Saint Elias Mountains', Elevation:'16,500 ft.'},
+    {rank:5, name:'Mount Blackburn', Range:'Wrangell Mountains', Elevation:'16,390 ft'},
+    {rank:6, name:'Mount Sanford', Range:'Wrangell Mountains', Elevation:'16,237 ft.'},
+    {rank:7, name:'Mount Fairweather', Range:'Saint Elias Mountains', Elevation:'15,325 ft.'},
+    {rank:8, name:'Mount Hubbard', Range:'Saint Elias Mountains', Elevation:'14,951 ft.'},
+    {rank:9, name:'Mount Bear', Range:'Saint Elias Mountains', Elevation:'14,831'},
+    {rank:10, name:'Mount Hunter', Range:'Alaska Range', Elevation:'14,573 ft.'}
 ]
